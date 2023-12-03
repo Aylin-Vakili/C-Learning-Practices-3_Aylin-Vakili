4b9fd10406ff2d76c4132e64cd9afb2866c46bb5
Aylin-Vakili <150549117+Aylin-Vakili@users.noreply.github.com>
2023-12-03 17:04:31 +0330

    Update Practice 3.c

--- Practice 3.c
+++ Practice 3.c
@@ -5,13 +5,13 @@
 { float a,b,c,delta,x2,x1 ;
     printf("Enter coefficients: ");
      scanf("%f %f %f", &a , &b , &c );
-    if (a<0){ a=-a , b=-b , c=-c }
+    if (a<0){ a=-a ; b=-b ; c=-c ; }
  delta = pow( b , 2 ) - 4*a*c ;
 
     if (delta < 0){printf ("Equation has no real roots\n");}
 
-    else { x1 = ( -b - sqrt(delta) / (2*a));
-           x2 = ( -b + sqrt(delta) / (2*a ));
+    else { x1 = ( (-b - sqrt(delta)) / (2*a));
+           x2 = ( (-b + sqrt(delta)) / (2*a ));
         printf("Roots are : %.2f , %.2f\n" , x1 , x2 );}
 
   return 0;
